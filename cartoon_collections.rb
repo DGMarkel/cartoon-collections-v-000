c2a813dc6c346de789b37a35f7d44e6cd1959f5e
David Markel <davidgmarkel@gmail.com>
2018-03-13 17:19:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map {|call| puts "#{call.capitalize}!}
+  planeteer_calls.map {|call| puts "#{call.capitalize}!""}
   puts planeteer_calls
 end
 
